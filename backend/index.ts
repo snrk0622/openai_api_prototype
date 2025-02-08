49de7fcb8107306c08b0bf9cc61dcfad6de995f6
snrk0622 <snrk.0622@gmail.com>
2025-02-08 21:34:57 +0900

    バックエンドで中断する処理追加

--- backend/index.ts
+++ backend/index.ts
@@ -35,6 +35,8 @@ app.get("/openai/chat/", async (c) => {
 });
 
 app.post("/openai/stream/", async (c) => {
+  console.log("Started '/openai/stream/' request")
+
   const { model = DEFAULT_MODEL, message } = await c.req.json();
   if (!message) {
     return c.json({ error: "message is required" }, 400);
@@ -49,14 +51,31 @@ app.post("/openai/stream/", async (c) => {
   return new Response(
     new ReadableStream({
       async start(controller) {
+        const fullResponse = {
+          model,
+          message: '',
+        }
+
         // 最初にモデル情報を返却
         controller.enqueue(JSON.stringify({ type: 'model', data: model }) + '\n' );
 
-        for await (const chunk of stream) {
-          const text = chunk.choices[0]?.delta?.content || '';
-          controller.enqueue(JSON.stringify({ type: 'text', data: text }) + '\n');
+        try {
+          for await (const chunk of stream) {
+            const text = chunk.choices[0]?.delta?.content || '';
+            controller.enqueue(JSON.stringify({ type: 'text', data: text }) + '\n');
+            fullResponse.message += text;
+          }
+          console.log("Finished '/openai/stream/' request successfully with response: ", fullResponse)
+        } catch (error) {
+          if (error instanceof Error && error.name === 'AbortError') {
+            stream.controller.abort();
+            console.log("Finished '/openai/stream/' request by abort")
+          } else {
+            console.error("Finished '/openai/stream/' request with error: ", error)
+          }
+        } finally {
+          controller.close();
         }
-        controller.close();
       },
     }),
     {
