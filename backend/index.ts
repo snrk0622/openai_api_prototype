046099dedc71f5a0705bfb1bb25e60938dfff461
snrk0622 <snrk.0622@gmail.com>
2025-02-08 10:56:19 +0900

    openai apiへリクエスト成功

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,25 @@
 import { Hono } from "hono";
+import { OpenAI } from "openai";
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello Hono.js"));
+
+app.get("/openai/chat/", async (c) => {
+  const { message } = c.req.query();
+  if (!message) {
+    return c.json({ error: "message is required" }, 400);
+  }
+
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [{ role: "user", content: message }],
+  });
+  return c.json(completion.choices[0].message.content);
+});
 
 export default {
   port: 3000,
